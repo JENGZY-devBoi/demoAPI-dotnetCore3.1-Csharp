f5ca0620d24981f50251b39556f935712927e51c
Teerapong Saisrikaew <teerapong.saisrikaew250819@gmail.com>
2022-10-22 16:38:14 +0700

    create user controller

---
+++ demoAPI/Controllers/Users.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demoAPI.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public String Get()
+    {
+        return "Hello";
+    }
+
+    [HttpGet("{id}")]
+    public String Get(string id)
+    {
+        return "Hello" + id;
+    }
+}
