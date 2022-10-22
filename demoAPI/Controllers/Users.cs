3b2277a2a99eadd2cb8c8c9c769415bafeccaa40
Teerapong Saisrikaew <teerapong.saisrikaew250819@gmail.com>
2022-10-22 16:44:02 +0700

    update

--- demoAPI/Controllers/Users.cs
+++ demoAPI/Controllers/Users.cs
@@ -1,20 +1,71 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using demoAPI.Models;
 
 namespace demoAPI.Controllers;
 
 [ApiController]
 [Route("/api/[controller]")]
-public class UserController : ControllerBase
+public class UsersController : ControllerBase
 {
     [HttpGet]
-    public String Get()
+    public IEnumerable<UserModel> Get()
     {
-        return "Hello";
+        List<UserModel> list = new List<UserModel>();
+        list.Add(new UserModel
+        {
+            Id = "001",
+            Name = "user1",
+            Age = 20
+        });
+        list.Add(new UserModel
+        {
+            Id = "002",
+            Name = "user2",
+            Age = 25
+        });
+        list.Add(new UserModel
+        {
+            Id = "003",
+            Name = "user3",
+            Age = 30
+        });
+
+        return list;
     }
 
     [HttpGet("{id}")]
-    public String Get(string id)
+    public UserModel Get(String id)
     {
-        return "Hello" + id;
+        return new UserModel
+        {
+            Id = id,
+            Name = "user1",
+            Age = 20
+        };
+    }
+
+    [HttpDelete("{id}")]
+    public UserModel Del(String id)
+    {
+        return new UserModel
+        {
+            Id = id,
+            Name = "Deleted",
+            Age = 20
+        };
+    }
+
+    [HttpPost]
+    public UserModel Create(UserModel data)
+    {
+        data.Name += " Created";
+        return data;
+    }
+
+    [HttpPut]
+    public UserModel Update(UserModel data)
+    {
+        data.Name += " Updated";
+        return data;
     }
 }
---
+++ demoAPI/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace demoAPI.Models
+{
+	public class UserModel
+	{
+		public String Id { get; set; }
+		public String Name { get; set; }
+		public int Age { get; set; }
+	}
+}
